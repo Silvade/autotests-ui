32c423fd1dfb2478ef57696d59b158d0d9daa504
Silvade <myanova13@gmail.com>
2026-06-15 22:35:39 +0300

    Вынесла работу с браузером в глобальный плагин

---
+++ fixtures/browsers.py
@@ -0,0 +1,43 @@
+import pytest
+from playwright.sync_api import Playwright, Page, expect
+
+
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright) -> None:
+    chromium = playwright.chromium.launch(headless=False)
+    context = chromium.new_context()
+    page = context.new_page()
+    page.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration"
+    )
+
+    registration_button = page.get_by_test_id("registration-page-registration-button")
+    expect(registration_button).to_be_disabled()
+
+    email_input = page.get_by_test_id("registration-form-email-input").locator("input")
+    email_input.fill("user.name@gmail.com")
+
+    username_input = page.get_by_test_id("registration-form-username-input").locator(
+        "input"
+    )
+    username_input.fill("username")
+
+    password_input = page.get_by_test_id("registration-form-password-input").locator(
+        "input"
+    )
+    password_input.fill("password")
+
+    expect(registration_button).to_be_enabled()
+
+    registration_button.click()
+    path = "browser-state.json"
+    context.storage_state(path=path)
+    chromium.close()
+
+
+@pytest.fixture()
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    chromium = playwright.chromium.launch(headless=False)
+    context = chromium.new_context(storage_state="browser-state.json")
+    yield context.new_page()
+    chromium.close()
